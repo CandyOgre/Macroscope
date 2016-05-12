3bd2ec174995efb14907923b3068cac941116f50
CandyOgre <no.one.coin@gmail.com>
2016-05-12 09:33:53 +0300

    Add validation using SimpleSchema

--- imports/api/posts/posts.js
+++ imports/api/posts/posts.js
@@ -1,9 +1,15 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const Posts = new Mongo.Collection('posts');
 
 Meteor.methods({
   'posts.remove'(postId) {
+  	new SimpleSchema({
+  	  postId: {type: String}
+  	}).validate({postId});
+
   	const post = Posts.findOne(postId);
   	Posts.remove(postId);
   },
